be3a3c21307aa6d149ecfb7861976812d2e03b0a
igor <igor@mail.com>
2021-04-17 01:06:27 +0300

    Добавил Service Discovery через eureka сервис, а также поиск config сервиса через eureka

--- business-app-1/src/main/java/com/business/app/one/BusinessAppOne.java
+++ business-app-1/src/main/java/com/business/app/one/BusinessAppOne.java
@@ -3,9 +3,11 @@ package com.business.app.one;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @RestController
 public class BusinessAppOne {
